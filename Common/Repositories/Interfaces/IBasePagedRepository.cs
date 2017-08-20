1136f328ad527d6d0a005324b56490025e139c21
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2017-08-20 17:26:53 +0300

    Created BaseRepository interfaces and abstract implementations

---
+++ Common/Repositories/Interfaces/IBasePagedRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FreeParkingSystem.Common.Models;
+using X.PagedList;
+
+// ReSharper disable CheckNamespace
+namespace FreeParkingSystem.Common.Repositories
+{
+    public interface IBasePagedRepository<TEntity> : IBaseRepository<TEntity> where TEntity : IBaseModel, new()
+    {
+
+        IPagedList<TEntity> GetByFilter(Func<TEntity, bool> filter, int skip, int take);
+
+        IPagedList<TEntity> GetByFilter<TKey>(Func<TEntity, bool> filter, Func<TEntity, TKey> orderBy, int skip, int take);
+    }
+}

4a21142b661963760c07ad2ba92f86d6e75f5813
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2017-08-23 02:03:58 +0300

    transfer the Data Annotations to the actual implementation

--- Common/Models/Interfaces/IUser.cs
+++ Common/Models/Interfaces/IUser.cs
@@ -1,30 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-// ReSharper disable CheckNamespace
+﻿// ReSharper disable CheckNamespace
 namespace FreeParkingSystem.Common.Models
 {
     public interface IUser : IBaseModel
     {
-        [Required]
+
         string FirstName { get; set; }
 
-        [Required]
         string LastName { get; set; }
 
-        [Required]
-        [EmailAddress]
         string Email { get; set; }
 
         string Password { get; set; }
 
-        [Phone]
         string Phone { get; set; }
 
         bool Active { get; set; }
 
         string FullName();
 
-        [Required]
         IRole Role { get; set; }
 
     }
--- Common/Models/User.cs
+++ Common/Models/User.cs
@@ -1,24 +1,32 @@
-﻿namespace FreeParkingSystem.Common.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeParkingSystem.Common.Models
 {
     public class User : BaseModel, IUser
     {
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Password { get; set; }
 
+        [Phone]
         public string Phone { get; set; }
 
         public bool Active { get; set; }
+
+        [Required]
+        public IRole Role { get; set; }
 
         public string FullName()
         {
             return $"{FirstName} {LastName}";
         }
-
-        public IRole Role { get; set; }
     }
 }

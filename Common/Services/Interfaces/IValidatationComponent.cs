62c3dd667b9f7c7c0d46e522e395fc49e0843343
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2017-08-23 12:26:55 +0300

    [WIP] Created Validation with The Composite pattern

---
+++ Common/Services/Interfaces/IValidatationComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace FreeParkingSystem.Common.Services
+{
+    public interface IValidatationComponent
+    {
+        /// <summary>
+        /// Validates the given object 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        IValidationResult Validate(object obj);
+    }
+}
---
+++ Common/Services/ValidationResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeParkingSystem.Common.Services
+{
+    public struct ValidationResult : IValidationResult
+    {
+        public bool IsValid { get; }
+
+        public IEnumerable<Exception> Errors { get; }
+
+        public ValidationResult(bool isValid, IEnumerable<Exception> errors = null)
+        {
+            IsValid = isValid;
+            Errors = errors;
+        }
+
+        public ValidationResult(IEnumerable<Exception> errors)
+        {
+            IsValid = errors?.Count() > 0;
+            Errors = errors;
+        }
+    }
+}

9ac440942c8b28af15a32ebe26d09a4f68d6eb0c
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2017-09-05 00:54:13 +0300

    created AccountService

---
+++ Common/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FreeParkingSystem.Common.Models;
+
+namespace FreeParkingSystem.Common.Services
+{
+    public interface IAccountService : IStoreService<IUser>
+    {
+        IServiceResult<IUser> FindByEmail(string email);
+
+        IServiceResult<IEnumerable<IUser>> GetUsersByRole(IRole role);
+
+        IServiceResult<IUser> ToggleActive(IUser user, bool active = false);
+
+        IServiceResult<IUser> CreateUser(string email, string firstName, string lastName, IRole role = null);
+
+        IServiceResult<IUser> CreatePassword(IUser user, string password);
+
+        IServiceResult<IUser> Update(IUser user);
+
+        IServiceResult<bool> Delete(IUser user);
+
+    }
+}

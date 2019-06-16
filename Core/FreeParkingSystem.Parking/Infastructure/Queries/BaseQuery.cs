34616d768e8a8f988024e0d6f0efdfaa6a9b50e0
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2019-06-16 17:05:30 +0100

    Clean up and start from scratch

--- Core/FreeParkingSystem.Parking/Infastructure/Queries/BaseQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FreeParkingSystem.Parking.Infastructure.Queries
-{
-    public abstract class BaseQuery<TQueryClass> where TQueryClass : class
-    {
-        public abstract IEnumerable<TQueryClass> Apply(IEnumerable<TQueryClass> query);
-    }
-}

413501908219c419d0cb1bd08c54e73bc57bcb3b
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2019-06-17 23:23:42 +0100

    Include Order table.
    Update Parking Site Active column to IsActive.
    Update ParkingSpot to include IsAvailable Column.

--- database/FreeParkingSystem.Database/Parking/ParkingSpot.sql
+++ database/FreeParkingSystem.Database/Parking/ParkingSpot.sql
@@ -5,6 +5,7 @@
     [ParkingSpotTypeId] INT NOT NULL, 
     [Position] INT NOT NULL, 
     [Level] INT NOT NULL, 
+	[IsAvailable] BIT NOT NULL DEFAULT 0, 	
     CONSTRAINT [FK_ParkingSpot_ToParkingSite] FOREIGN KEY ([ParkingSiteId]) REFERENCES [ParkingSite]([Id]), 
     CONSTRAINT [AK_ParkingSpot_UniquePosition] UNIQUE ([ParkingSiteId], [Level], [Position]), 
     CONSTRAINT [FK_ParkingSpot_ToParkingSpotType] FOREIGN KEY ([ParkingSpotTypeId]) REFERENCES [ParkingSpotType]([Id])

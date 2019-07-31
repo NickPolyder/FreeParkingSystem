25ba92e2c4097eb5c602507da56dcd969c4e1fa4
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2019-07-31 23:26:45 +0100

    Start working with Repositories

---
+++ src/Account/FreeParkingSystem.Accounts.Contract/Repositories/IClaimsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using FreeParkingSystem.Common;
+
+namespace FreeParkingSystem.Accounts.Contract.Repositories
+{
+	public interface IClaimsRepository: IRepository<UserClaim>
+	{
+		IEnumerable<UserClaim> GetClaimsByUser(int userId);
+
+	}
+}

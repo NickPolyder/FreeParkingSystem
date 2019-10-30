bf0db4a485d0ff19c76c527bd10bd7a54c37561c
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2019-10-30 23:47:06 +0000

    Create the first endpoint of ParkingSite Add

---
+++ src/Parking/FreeParkingSystem.Parking.Contract/Dtos/AddParkingSiteInput.cs
@@ -0,0 +1,11 @@
+﻿namespace FreeParkingSystem.Parking.Contract.Dtos
+{
+	public class AddParkingSiteInput
+	{
+		public string Name { get; set; }
+
+		public int ParkingTypeId { get; set; }
+
+		public Geolocation Geolocation { get; set; }
+	}
+}
